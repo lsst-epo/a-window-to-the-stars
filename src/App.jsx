50dfdc737034f4c82e1a4a1a2e777b517c9e0fee
Blake Mason <bmason@lsst.org>
2019-06-18 12:58:07 -0700

    [F] Add basic routing using react-router
    Header nav with links to three available charts;
    Basic header styles;
    Add react-router to package.json;
    Further tweaks to eslint/prettier rules for smoother build step;
    
    EPO-1869

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader/root';
+import { Route, Link, HashRouter as Router } from 'react-router-dom';
 import ScatterPlot from './components/scatter-plot';
 import Histogram from './components/histogram';
 import stars from './assets/static-data/stars.json';
@@ -12,45 +13,75 @@ class App extends Component {
 
   render() {
     return (
-      <main>
-        <h1
-          style={{
-            position: 'absolute',
-            left: '10%',
-            top: '20px',
-          }}
-        >
-          Stellar Graphs
-        </h1>
-        <Histogram
-          width={window.innerWidth}
-          height={window.innerHeight}
-          padding={80}
-          data={stars}
-          valueAccessor="teff"
-          xAxisLabel="Temperature (K)"
-          yAxisLabel="Count"
-        />
-        <Histogram
-          width={window.innerWidth}
-          height={window.innerHeight}
-          padding={80}
-          data={stars}
-          valueAccessor="luminosity"
-          xAxisLabel="Luminosity"
-          yAxisLabel="Count"
-        />
-        <ScatterPlot
-          width={window.innerWidth}
-          height={window.innerHeight}
-          padding={80}
-          data={stars}
-          xValueAccessor="teff"
-          yValueAccessor="luminosity"
-          xAxisLabel="Temperature (K)"
-          yAxisLabel="Luminosity"
-        />
-      </main>
+      <Router>
+        <header className="header-primary">
+          <h1>Stellar Graphs</h1>
+          <nav className="nav-primary">
+            <ul>
+              <li>
+                <Link to="/">H-R Diagram</Link>
+              </li>
+              <li>
+                <Link to="/temperature">Temperature Histogram</Link>
+              </li>
+              <li>
+                <Link to="/luminosity">Luminosity Histogram</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <Route
+            exact
+            path="/"
+            render={routeProps => (
+              <ScatterPlot
+                {...routeProps}
+                width={window.innerWidth}
+                height={window.innerHeight - 100}
+                padding={80}
+                data={stars}
+                xValueAccessor="teff"
+                yValueAccessor="luminosity"
+                xAxisLabel="Temperature (K)"
+                yAxisLabel="Luminosity"
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/temperature"
+            render={routeProps => (
+              <Histogram
+                {...routeProps}
+                width={window.innerWidth}
+                height={window.innerHeight - 100}
+                padding={80}
+                data={stars}
+                valueAccessor="teff"
+                xAxisLabel="Temperature (K)"
+                yAxisLabel="Count"
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/luminosity"
+            render={routeProps => (
+              <Histogram
+                {...routeProps}
+                width={window.innerWidth}
+                height={window.innerHeight - 100}
+                padding={80}
+                data={stars}
+                valueAccessor="luminosity"
+                xAxisLabel="Luminosity"
+                yAxisLabel="Count"
+              />
+            )}
+          />
+        </main>
+      </Router>
     );
   }
 }
