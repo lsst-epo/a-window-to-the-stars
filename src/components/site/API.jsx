5fd0879679304b55201d807076c86541766ba10e
Blake Mason <bmason@lsst.org>
2019-08-09 15:03:38 -0700

    [B] Fix env variables for git vs local vs netlify

--- src/components/site/API.jsx
+++ src/components/site/API.jsx
@@ -1,19 +1,15 @@
 import axios from 'axios';
 
-// const getBaseUrl = function(env, target) {
-//   console.log(env);
-//   if (env === 'production' && target === 'staging') {
-//     return 'https://lsst-epo.github.io/a-window-to-the-stars/';
-//   }
+const getBaseUrl = function(env) {
+  const envs = {
+    development: '/',
+    staging: '/',
+    production: 'https://lsst-epo.github.io/a-window-to-the-stars/',
+  };
 
-//   if (env === 'production') {
-//     return 'https://lsst-epo.github.io/a-window-to-the-stars/';
-//   }
-
-//   return 'http://localhost:8080/';
-// };
+  return envs[env] || '/';
+};
 
 export default axios.create({
-  baseURL: '/',
-  // baseURL: getBaseUrl(process.env.NODE_ENV, process.env.DEPLOY_ENV),
+  baseURL: getBaseUrl(process.env.NODE_ENV),
 });
