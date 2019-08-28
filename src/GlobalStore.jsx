29e057ec95db0bc8ff8741ccad7aa3218949c819
Blake Mason <bmason@lsst.org>
2019-08-28 15:06:57 -0700

    [B] Combined vs Combining conditionally render
    Previously the condition for rendeirng Combined vs Combining page was
    checking to see if the page had been visited before.  This was always
    triggering true because the visitedPages global state value was
    evaluated immediately before/as the page loaded.  Now it checks to see
    if the user has visited any page > the page in question.
    
    After much deliberating: used this as an opprotunity to refactor Global
    state stuff into its own class, and instantiate that class, and call
    some of its class functions in index.jsx before rendering App component
    so state is already available.

---
+++ src/GlobalStore.jsx
@@ -0,0 +1,78 @@
+import { addCallback, addReducer, setGlobal } from 'reactn';
+import ls from 'local-storage';
+import includes from 'lodash/includes';
+import sortBy from 'lodash/sortBy';
+
+class GlobalStore {
+  constructor() {
+    this.emptyState = {
+      lastUpdated: Date.now().toString(),
+      questions: null,
+      answers: {},
+      totalNumPages: 18,
+      visitedPages: [],
+      investigationProgress: 0,
+      pageProgress: 0,
+      activeId: null,
+      activeGraphData: null,
+      clusterA: [],
+      clusterB: [],
+    };
+
+    // const existingState = this.emptyState;
+    const existingState = ls('hrd') || this.emptyState;
+
+    setGlobal(existingState);
+  }
+
+  addCallbacks() {
+    addCallback(global => {
+      ls('hrd', global);
+      return null;
+    });
+  }
+
+  addReducers() {
+    addReducer('empty', prevGlobal => {
+      const global = {
+        ...prevGlobal,
+        lastUpdated: Date.now().toString(),
+        answers: {},
+        activeId: null,
+        activeGraphData: null,
+        visitedPages: [],
+        investigationProgress: 0,
+        pageProgress: 0,
+      };
+
+      ls('hrd', global);
+
+      return global;
+    });
+
+    addReducer('updateProgress', (prevGlobal, dispatch, currentProgress) => {
+      const { visitedPages: prevVisitedPages, totalNumPages } = prevGlobal;
+      const pageProgress = Math.ceil(
+        (currentProgress / (totalNumPages - 1)) * 100
+      );
+
+      const global = {
+        ...prevGlobal,
+        pageProgress,
+      };
+
+      if (!includes(prevVisitedPages, currentProgress)) {
+        const visitedPages = sortBy(prevVisitedPages.concat([currentProgress]));
+        const investigationProgress = Math.ceil(
+          (visitedPages.length / totalNumPages) * 100
+        );
+        global.visitedPages = visitedPages;
+        global.investigationProgress = investigationProgress;
+      }
+
+      return global;
+    });
+  }
+}
+
+export default GlobalStore;
