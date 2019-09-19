c3cb978401d7c9ba2168ec978c486ba1f249834b
Blake Mason <bmason@lsst.org>
2019-09-19 15:09:27 -0700

    [F] Update Exploring sections sequencing
    Curious to break up some of these longer sections into more managable
    sets of questions.  This "creating regions on th eH-R diagram based on
    selections flow" seems like a great place to start.

---
+++ src/components/content/sections/copy/ExploringGiants.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function ExploringGiantsCopy() {
+  return (
+    <React.Fragment>
+      <p>
+        There are also groups of points above the main sequence representing
+        giant stars. Giant stars are stars in the late stages of their
+        lifetimes. They are powered by more than one type of nuclear fusion
+        reaction.
+      </p>
+      <p>
+        Answer the following questions to determine the approximate position of
+        the Giant stars on this H-R Diagram. As you answer questions this region
+        will be added to the H-R Diagram.
+      </p>
+    </React.Fragment>
+  );
+}
+
+export default ExploringGiantsCopy;
---
+++ src/components/content/sections/copy/ExploringMainSequence.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function ExploringMainSequenceCopy() {
+  return (
+    <React.Fragment>
+      <p>
+        An H-R Diagram is a plot that displays temperature and luminosity
+        information about stars. Luminosity (Y axis) is a measure of the total
+        energy output from a star per unit of time. It’s commonly expressed as
+        Solar luminosity, which is the ratio of a star’s energy output compared
+        to the energy output of the Sun. For example, a star with a solar
+        luminosity of 10 is giving off ten times more energy than the Sun. The
+        temperature (X axis) is commonly measured in Kelvin.
+      </p>
+      <p>
+        Notice that most data points occupy a region stretching diagonally from
+        the upper left to the lower right of the plot. This region is called the
+        main sequence. Stars in the main sequence are fusing hydrogen. The
+        fusion pressure force is balanced against the force of gravity, which
+        keeps the star in a stable state for most of its lifetime.
+      </p>
+      <p>
+        The main sequence forms a slightly s-shaped diagonal band across the
+        plot. Answer the following questions to determine the approximate
+        position of the main sequence stars on this H-R Diagram. As you answer
+        questions this region will be added to the H-R Diagram.
+      </p>
+    </React.Fragment>
+  );
+}
+
+export default ExploringMainSequenceCopy;
