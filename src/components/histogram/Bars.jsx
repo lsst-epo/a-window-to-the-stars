1d9bbd69a0d5ca02a3aa9ecc2dca0ddb8ee613bc
Blake Mason <bmason@lsst.org>
2019-09-27 16:47:37 -0700

    [F] Increase interactive-able area for Bars
    In Histogram, each columnar area bar is situated in represents the
    clickable/hoverable area (instead of the event target being the bar
    only). THis alleviates some of the problems associated with extra small
    bins: hard to interact with.  It does not address "quality" of binning.

--- src/components/histogram/Bars.jsx
+++ src/components/histogram/Bars.jsx
@@ -17,21 +17,25 @@ class Bars extends React.PureComponent {
       xScale,
       yScale,
       offsetTop,
+      graphHeight,
+      padding,
       barClasses,
     } = this.props;
-
     return (
       <g className="data-bars">
         {data.map((d, i) => {
           const key = `bar-${i}`;
+          const barHeight = yScale(0) - yScale(d.length);
 
           return (
             <Bar
               key={key}
               x={xScale(d.x0)}
               y={yScale(d.length) + offsetTop}
+              absentY={offsetTop}
               width={xScale(d.x1) - xScale(d.x0)}
-              height={yScale(0) - yScale(d.length)}
+              height={barHeight}
+              absentHeight={graphHeight - padding - barHeight - 1}
               classes={barClasses}
               selected={this.isMatch(selectedData, d)}
               hovered={this.isMatch(hoveredData, d)}
@@ -48,6 +52,8 @@ Bars.propTypes = {
   selectedData: PropTypes.bool,
   hoveredData: PropTypes.bool,
   offsetTop: PropTypes.number,
+  graphHeight: PropTypes.number,
+  padding: PropTypes.number,
   xScale: PropTypes.func,
   yScale: PropTypes.func,
   barClasses: PropTypes.string,
